d34d78e9ae306a715ea8ce57075806d2fd387b82
ENTELECT\bashirat.sunmola <bashirat.sunmola@entelect.co.za>
2023-05-11 17:04:43 +0200

    Update api documentation for customer and flight service

--- customer-service/src/main/java/entelect/training/incubator/spring/customer/controller/CustomersController.java
+++ customer-service/src/main/java/entelect/training/incubator/spring/customer/controller/CustomersController.java
@@ -4,6 +4,12 @@ import entelect.training.incubator.spring.customer.model.Customer;
 import entelect.training.incubator.spring.customer.model.CustomerSearchRequest;
 import entelect.training.incubator.spring.customer.model.SearchType;
 import entelect.training.incubator.spring.customer.service.CustomersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -24,6 +30,12 @@ public class CustomersController {
         this.customersService = customersService;
     }
 
+    @Operation(summary="Create a new customer")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Customer created",
+                content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Customer.class)) })
+                })
     @PostMapping
     public ResponseEntity<?> createCustomer(@RequestBody Customer customer) {
         LOGGER.info("Processing customer creation request for customer={}", customer);
@@ -34,6 +46,15 @@ public class CustomersController {
         return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
     }
 
+    @Operation(summary="Find all customers")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found customers",
+                content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Customer.class)) }),
+        @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                content = @Content),
+        @ApiResponse(responseCode = "404", description = "No customers could be found",
+                content = @Content) })
     @GetMapping
     public ResponseEntity<?> getCustomers() {
         LOGGER.info("Fetching all customers");
@@ -48,8 +69,17 @@ public class CustomersController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Find customer by id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found customer",
+                content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Customer.class)) }),
+        @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                content = @Content),
+        @ApiResponse(responseCode = "404", description = "Customer not found",
+                content = @Content) })
     @GetMapping("{id}")
-    public ResponseEntity<?> getCustomerById(@PathVariable Integer id) {
+    public ResponseEntity<?> getCustomerById(@Parameter(description = "id of customer to get") @PathVariable Integer id) {
         LOGGER.info("Processing customer search request for customer id={}", id);
         Customer customer = this.customersService.getCustomer(id);
 
@@ -62,6 +92,15 @@ public class CustomersController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Search customers by user name, first name or passport number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found customers",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Customer.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid search parameters",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Customer not found",
+                    content = @Content) })
     @PostMapping("/search")
     public ResponseEntity<?> searchCustomers(@RequestBody CustomerSearchRequest searchRequest) {
         LOGGER.info("Processing customer search request for request {}", searchRequest);
--- flight-service/src/main/java/entelect/training/incubator/spring/flight/controller/FlightsController.java
+++ flight-service/src/main/java/entelect/training/incubator/spring/flight/controller/FlightsController.java
@@ -3,6 +3,11 @@ package entelect.training.incubator.spring.flight.controller;
 import entelect.training.incubator.spring.flight.model.Flight;
 import entelect.training.incubator.spring.flight.model.FlightsSearchRequest;
 import entelect.training.incubator.spring.flight.service.FlightsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -28,6 +33,11 @@ public class FlightsController {
         this.flightsService = flightsService;
     }
 
+    @Operation(summary = "Create a new flight")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Flight created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Flight.class)) })})
     @PostMapping
     public ResponseEntity<?> createFlight(@RequestBody Flight flight) {
         LOGGER.info("Processing flight creation request for flight={}", flight);
@@ -38,6 +48,13 @@ public class FlightsController {
         return new ResponseEntity<>(savedFlight, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Fetch all flights")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found flights",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Flight.class)) }),
+            @ApiResponse(responseCode = "404", description = "No flights found",
+                    content = @Content) })
     @GetMapping()
     public ResponseEntity<?> getFlights() {
         LOGGER.info("Fetching all flights");
@@ -52,6 +69,15 @@ public class FlightsController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Find flight by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found flight",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Flight.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Flight not found",
+                    content = @Content) })
     @GetMapping("{id}")
     public ResponseEntity<?> getFlightById(@PathVariable Integer id) {
         LOGGER.info("Processing flight search request for flight id={}", id);
@@ -66,6 +92,15 @@ public class FlightsController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Find flight by departure date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found flight or flights",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Flight.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid search parameters",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Flights not found",
+                    content = @Content) })
     @PostMapping("/search")
     public ResponseEntity<?> searchFlights(@RequestBody FlightsSearchRequest searchRequest) {
         LOGGER.info("Processing flight search request: {}", searchRequest);
@@ -81,6 +116,13 @@ public class FlightsController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Find discounted flights")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found discounted flights",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Flight.class)) }),
+            @ApiResponse(responseCode = "404", description = "No discounted flights found",
+                    content = @Content) })
     @GetMapping("/specials")
     public List<Flight> getFlightSpecials() {
         LOGGER.info("Processing flight specials request");
